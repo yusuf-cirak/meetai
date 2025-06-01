83fb8593d128130288ad55e12b01134f5de41c88
Yusuf Çırak <yusuf10901@hotmail.com>
2025-06-01 18:30:38 +0300

    12: agents data table

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,13 +3,16 @@ import { agents } from "@/db/schema";
 import {  createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import z from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
     getOne : protectedProcedure.input(z.object({id:z.string()})).query(async ({input}) => {
 
         const [agent] = await db
-            .select()
+            .select({
+                ...getTableColumns(agents),
+                meetingCount : sql<number>`5`,
+            })
             .from(agents)
             .where(eq(agents.id, input.id));
 
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,6 +4,9 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
     const trpc = useTRPC();
@@ -23,7 +26,13 @@ export const AgentsView = () => {
     //     return <ErrorState title="Error when loading agents" description="Something went wrong"></ErrorState>
     // }
 
-    return (<div></div>)
+    return (<div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <DataTable columns={columns} data={data}/>
+
+        {data.length === 0 && (
+            <EmptyState title="Create your first agent" description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call."></EmptyState>
+        )}
+    </div>)
 }
 
 
